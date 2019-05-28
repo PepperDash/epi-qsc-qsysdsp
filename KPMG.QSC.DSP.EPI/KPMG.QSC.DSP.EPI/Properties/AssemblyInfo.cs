3938335f12328bad5e53643ec99cd97e3601c1eb
Joshua Gutenplan <jgutenplan@pepperdash.com>
2019-05-28 12:34:34 -0700

    add qsc classes from Essentials to build plugins from

---
+++ KPMG.QSC.DSP.EPI/KPMG.QSC.DSP.EPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("KPMG.QSC.DSP.EPI")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KPMG.QSC.DSP.EPI")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
